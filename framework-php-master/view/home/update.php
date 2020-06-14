144f70599eaa55fd42d9e8f604bd8d93af14b314
Merijn <99054535@mydavinci.nl>
2020-06-14 17:39:31 +0200

    ik ga morgen verder niet optijd klaar weet hiervan jammer

--- framework-php-master/view/home/update.php
+++ framework-php-master/view/home/update.php
@@ -2,7 +2,7 @@
 	<form name="update" method="post" action="<?=URL?>home/edit">
 	    <input type="hidden" name="id" value="<?=$paarden["id"] ?>"/>
 	    <!--  Bouw hier de rest van je formulier   -->
-		<label for='name'>Name : </label><input name='name' type='text' placeholder='<?= $paarden['name'] ?>'>
+		<label for='name'>Name : </label><input name='name' type='text' placeholder='<?= $paarden['namePaard'] ?>'>
 		<label for='age'>Leeftijd : </label><input name='age' type='text' placeholder='<?= $paarden['age'] ?>'>
 		<input type='submit' value='Update'>
 	</form>
